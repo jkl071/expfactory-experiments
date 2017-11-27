3624744228dc0502be3585b3a7188b9a4d19abdc
Jamie Li <jamie@DN0a22125e.SUNet>
2017-11-26 16:48:58 -0800

    changed cue control food for only 44 stims

--- cue_control_food/experiment.js
+++ cue_control_food/experiment.js
@@ -37,7 +37,6 @@ var createStims = function(numStims,numIterations,numZeroes){
 	
 function createAllStims(numStimsPerCategory, numIterations, numZeroes, nullType){
 	
-	var valued_stim_array = createStims(numStimsPerCategory, numIterations, numZeroes)
 	var neutral_stim_array = jsPsych.randomization.repeat(neutral_pics,1)
 
 	
@@ -99,7 +98,7 @@ function getRestText(){
 	}else if(currBlock < numBlocks - 1){	
 		return '<div class = bigbox><div class = centerbox>'+
 		  	  	 '<p class = center-textJamie style="font-size:36px"><font color="white">Take a break!</font></p>'+
-		      	 '<p class = center-textJamie style="font-size:36px"><font color="white">The task will start again in 20 seconds.</font></p>'+
+		      	 '<p class = center-textJamie style="font-size:36px"><font color="white">The task will start again in 10 seconds.</font></p>'+
 		 	   '</div></div>'
 	}
 
@@ -111,6 +110,10 @@ function getCue(){
 	whichCue = stims.trial_cue_type.pop()
 	stim_type = stims.stim_type.pop()
 	current_stim = stims.stim.pop()
+	
+	console.log('cue = '+whichCue)
+	console.log('stim_type = '+stim_type)
+	console.log('stim = '+current_stim)
 	
 	if (whichCue == "null"){
 	
@@ -131,7 +134,7 @@ function getProbe(){
 	
 	if(stim_type == experiment_stim_type){
 	
-		return probeBoard1 + valued_stim_directory + current_stim + fileTypeJPG + probeBoard2		
+		return probeBoard1 + valued_stim_directory + current_stim + "'></img>" + probeBoard2		
 	}else if(stim_type == "neutral"){
 
 		return probeBoard1 + neutral_directory + current_stim + "'></img>" + probeBoard2	
@@ -145,39 +148,18 @@ function getProbe(){
 
 
 function getRatingBoard(){	
-	console.log(current_stim)
 	
-	if(current_game_state == 'training'){
 	
 		if(stim_type == experiment_stim_type){
 	
-			return ratingBoard1 + valued_stim_directory + current_stim + fileTypeJPG + ratingBoard2		
+			return probeBoard1 + valued_stim_directory + current_stim + "'></img>" + probeBoard2	
 		} else if(stim_type == "neutral"){
 
 			return ratingBoard1 + neutral_directory + current_stim + "'></img>" + ratingBoard2	
 		} else if (stim_type == "null"){
 			
 			return '<div class = bigbox></div>'
-	}
-	
-		
-	}else if(current_game_state == 'post_rating'){
-		
-		
-		current_stim = post_rating_stims.stim.pop()
-		whichCue =  post_rating_stims.trial_cue_type.pop()
-		stim_type = post_rating_stims.stim_type.pop()
-		
-		if(stim_type == experiment_stim_type){
-	
-			return ratingBoard1 + valued_stim_directory + current_stim + fileTypeJPG + ratingBoard2		
-		}else if(stim_type == "neutral"){
-
-			return ratingBoard1 + neutral_directory + current_stim + "'></img>" + ratingBoard2	
 		}
-		
-	
-	}
 	
 	
 }
@@ -200,11 +182,11 @@ document.addEventListener("keydown", function(e){
     trigger_timer.push(time)
     }
     
+    
+    
 });
 
 var appendData = function(){
-	//curr_trial = jsPsych.progress().current_trial_global
-	//trial_id = jsPsych.data.getDataByTrialIndex(curr_trial).trial_id
 
 	jsPsych.data.addDataToLastTrial({
 		triggers: trigger_tracker,
@@ -213,20 +195,33 @@ var appendData = function(){
 
 	trigger_tracker = []
 	trigger_timer = []
+	
+	curr_trial = jsPsych.progress().current_trial_global
+	trial_id = jsPsych.data.getDataByTrialIndex(curr_trial).trial_id
+	
+	if (trial_id == "current_rating"){
+		jsPsych.data.addDataToLastTrial({
+			stim: current_stim,
+			stim_type: stim_type,
+			which_cue: whichCue
+			
+		})
+	
+	}
 }
 
 /* ************************************ */
 /*    Define Experimental Variables     */
 /* ************************************ */
 var subject_ID = 472
-var numStimsPerCategory = 50
+var numStimsPerCategory = 44
 var totalStims = (numStimsPerCategory/2) * 5 // 5 total conditions
 
 
 
 
 var numIterations = 1 //number of a certain stim, in each of the one conditions
-var numBlocks = totalStims / 25
+var numBlocks = totalStims / 22
 var numStimsPerBlock = totalStims / numBlocks
 
 var submitPressMax = 5
@@ -244,7 +239,6 @@ var postRateNullType = 0
 var preFileType = "<img class = center src='/static/experiments/cue_control_food/images/"
 
 
-var pathSource = "/static/experiments/cue_control_food/images/stim_numbered/"
 var valued_stim_directory = "PDC3_chosen_food_500/" //controls if you are showing food or smoking pictures
 var neutral_directory = "neutral_stims_500/"
 
@@ -260,26 +254,6 @@ var currBlock = 0
 var trigger_tracker = []
 var trigger_timer = []
 var submitPress = 0
-
-
-
-
-
-var preloadStimNumbered = createStims(60, 1, 4) //numStims
-var preloadControlNonFood = createStims(30, 1, 4)
-
-var images = []
-for(var i=0;i<preloadStimNumbered.length;i++){
-	images.push(pathSource + preloadStimNumbered[i] + ".bmp")
-}
-
-for(var i=0;i<preloadControlNonFood.length;i++){
-	images.push("/static/experiments/cue_control_food/images/control_non_food/" + preloadControlNonFood[i] + ".bmp")
-}
-
-
-jsPsych.pluginAPI.preloadImages(images);
-
 
 
 
@@ -305,11 +279,11 @@ var ratingBoard2 =
 	    '</div>'+
 		
 		'<div class = buttonbox>'+
-			'<div class = inner><button type="submit" class="likert_btn" onClick="return false;" >1</button></div>'+
-			'<div class = inner><button type="submit" class="likert_btn" onClick="return false;" >2</button></div>'+
-			'<div class = inner><button type="submit" class="likert_btn" onClick="return false;" >3</button></div>'+
-			'<div class = inner><button type="submit" class="likert_btn" onClick="return false;" >4</button></div>'+
-			'<div class = inner><button type="submit" class="likert_btn" onClick="return false;" >5</button></div>'+
+			'<div class = inner><button type="submit" class="likert_btn" id="btn1" onClick="return false;" >1</button></div>'+
+			'<div class = inner><button type="submit" class="likert_btn" id="btn2" onClick="return false;" >2</button></div>'+
+			'<div class = inner><button type="submit" class="likert_btn" id="btn3" onClick="return false;" >3</button></div>'+
+			'<div class = inner><button type="submit" class="likert_btn" id="btn4" onClick="return false;" >4</button></div>'+
+			'<div class = inner><button type="submit" class="likert_btn" id="btn5" onClick="return false;" >5</button></div>'+
 		'</div>'+	
 	'</div>'
 	
@@ -332,11 +306,11 @@ var testratingBoard3 =
 	    '</div>'+
 		
 		'<div class = buttonbox>'+
-			'<div class = inner><button type="submit" class="likert_btn" onClick="return false;" >1</button></div>'+
-			'<div class = inner><button type="submit" class="likert_btn" onClick="return false;" >2</button></div>'+
-			'<div class = inner><button type="submit" class="likert_btn" onClick="return false;" >3</button></div>'+
-			'<div class = inner><button type="submit" class="likert_btn" onClick="return false;" >4</button></div>'+
-			'<div class = inner><button type="submit" class="likert_btn" onClick="return false;" >5</button></div>'+
+			'<div class = inner><button type="submit" class="likert_btn" id="btn1" onClick="return false;" >1</button></div>'+
+			'<div class = inner><button type="submit" class="likert_btn" id="btn2" onClick="return false;" >2</button></div>'+
+			'<div class = inner><button type="submit" class="likert_btn" id="btn3" onClick="return false;" >3</button></div>'+
+			'<div class = inner><button type="submit" class="likert_btn" id="btn4" onClick="return false;" >4</button></div>'+
+			'<div class = inner><button type="submit" class="likert_btn" id="btn5" onClick="return false;" >5</button></div>'+
 		'</div>'+	
 	'</div>'
 	
@@ -382,30 +356,6 @@ var welcome_block = {
 	on_finish: appendData
 };
 
-var post_rating_intro = {
-	type: 'poldrack-text',
-	data: {
-		trial_id: "post_rating_intro"
-	},
-	timing_response: -1,
-	text: '<div class = bigbox><div class = centerbox>'+
-		  '<p class = center-textJamie style="font-size:36px"><font color="white">For this next phase, please rate how much you want to consume the item.</font></p>'+
-		  '<p class = center-textJamie style="font-size:36px"><font color="white">1 = very low, 5 = very high.</font></p>'+		  
-		  '</div></div>',
-	cont_key: [13],
-	timing_post_trial: 0,
-	on_finish: function(){
-		jsPsych.data.addDataToLastTrial({
-		triggers: trigger_tracker,
-		trigger_time: trigger_timer
-		})
-
-		trigger_tracker = []
-		trigger_timer = []
-		
-		current_game_state = "post_rating"
-	}
-};
 
 var experimentor_wait_block = {
 	type: 'poldrack-text',
@@ -416,11 +366,27 @@ var experimentor_wait_block = {
 	text: '<div class = bigbox><div class = centerbox>'+
 		  '<p class = center-textJamie style="font-size:36px"><font color="white">Scanner Setup.</font></p>'+
 		  '</div></div>',
-	cont_key: [84],
+	cont_key: [13],
 	timing_post_trial: 0,
-	on_trial_finish: appendData
+	on_finish: appendData
 };
 
+var scanner_wait_block_first = {
+	type: 'poldrack-single-stim',
+	stimulus: '<div class = bigbox><div class = centerbox>'+
+		  	  '<p class = center-textJamie style="font-size:36px"><font color="white">Task about to start!</font></p>'+
+		  	  '</div></div>',
+	is_html: true,
+	choices: [84],
+	data: {
+		trial_id: "scanner_wait"
+	},
+	timing_post_trial: 0,
+	timing_stim: -1,
+	timing_response: -1,
+	response_ends_trial: true,
+	on_finish: appendData
+};
 
 
 var scanner_wait_block = {
@@ -437,17 +403,7 @@ var scanner_wait_block = {
 	timing_stim: 10880,
 	timing_response: 10880,
 	response_ends_trial: false,
-	on_trial_finish: function(){
-		jsPsych.data.addDataToLastTrial({
-		triggers: trigger_tracker,
-		trigger_time: trigger_timer
-		})
-
-		trigger_tracker = []
-		trigger_timer = []
-		
-		current_game_state = "training"
-	}
+	on_finish: appendData
 };
 
 
@@ -539,10 +495,8 @@ for(var i = 0; i < numStimsPerBlock; i++){ //numStims before, should be # of tri
 	response_ends_trial: true
 	};
 	
-	//training_trials.push(fixation_block)
 	training_trials.push(cue_block)
 	training_trials.push(probe_block)
-	//training_trials.push(delay_block)
 	training_trials.push(rating_block)
 }
 training_trials.push(scanner_rest_block)
@@ -567,36 +521,6 @@ var training_node = {
 	}
 }
 
-
-var post_rating_trials = []
-for(var i = 0; i < 60; i++){ //numStims before, but probably should equal the number of all stims = 120
-	var rating_block = {
-	type: 'poldrack-single-stim',
-	stimulus: getRatingBoard,
-	is_html: true,
-	choices: [13],
-	data: {
-		trial_id: "post_rating"
-	},
-	timing_post_trial: 0,
-	timing_stim: 3000,
-	timing_response: 3000,
-	on_finish: appendData
-	};
-	
-	//post_rating_trials.push(fixation_block)
-	post_rating_trials.push(rating_block)
-}
-
-var post_rating_node = {
-	timeline: post_rating_trials,
-	loop_function: function(data){
-		
-	
-	}
-}
-
-
 /* ************************************ */
 /*          Set up Experiment           */
 /* ************************************ */
@@ -609,14 +533,12 @@ cue_control_food_experiment.push(instructions_block);
 
 cue_control_food_experiment.push(experimentor_wait_block);
 
+cue_control_food_experiment.push(scanner_wait_block_first);
+
 cue_control_food_experiment.push(scanner_wait_block);
 
 cue_control_food_experiment.push(training_node);
-/*
-cue_control_food_experiment.push(post_rating_intro);
 
-cue_control_food_experiment.push(post_rating_node);
-*/
 cue_control_food_experiment.push(end_block);
 
 
